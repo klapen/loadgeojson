112618a5f9534ad5d8c7a4480cea3611caed82cd
klapen <oasaavedrac@gmail.com>
2017-08-29 16:14:48 -0500

    Add file download button && center map on load

--- src/js/load.js
+++ src/js/load.js
@@ -1,11 +1,11 @@
-var oas;
+var data;
 document.addEventListener('DOMContentLoaded', function(){
     // Load map
     var width = 690,
  	height = 800;
     var map_url = "data/barrios.geo.json";
     var feature_classname = 'barrios';
-    var id_prop = 'cod_polbar';
+    var id_prop = 'objectid';
     
     var projection = d3.geoLittrow();
     var path = d3.geoPath().projection(projection);
@@ -13,19 +13,66 @@ document.addEventListener('DOMContentLoaded', function(){
     var svg = d3.select("#map").append("svg")
  	.attr("width", width)
  	.attr("height", height);
-    oas = svg;
+    
     d3.json(map_url, function(error,shapes) {
- 	svg.selectAll('path')
+	data = shapes.features;
+ 	var map = svg.append('g').attr('id','svg-map');
+	var paths = map.selectAll('path')
  	    .data(shapes.features)
  	    .enter()
  	    .append('path')
  	    .attr('class',feature_classname)
  	    .attr('id',function(d){return d.properties[id_prop]})
  	    .attr('d', path)
-	    .attr('transform',
-		  'translate(-607820.5885038204,-439749.6151207932) scale(1782.8875536304574)');
- 	svg.call(d3.zoom().scaleExtent([1, 2000]).on("zoom", function(){
- 	    svg.selectAll('path').attr('transform', d3.event.transform);
- 	}));
+	var dim = document.getElementById('svg-map').getBBox();
+	var scale = dim.height > dim.width ? height/dim.height : width/dim.width;
+	console.log(dim,dim.x*scale,dim.y*scale,scale);
+	paths.attr('transform','translate(-'+dim.x*scale+',-'+dim.y*scale+') scale('+scale+')');
+
+	// ToDo: set event transform to transform values
+ 	//svg.call(d3.zoom().scaleExtent([1, 3000]).on("zoom", function(){
+ 	//    svg.selectAll('path').attr('transform', d3.event.transform);
+ 	//}));
     });
 });
+
+// Utils
+var utils = {
+    translation: function (x,y) {return 'translate(' + x + ',' + y + ')';}, 
+    widthCalc: function (id){return document.getElementById(id).clientWidth;},
+    heightCalc: function (id){return document.getElementById(id).clientHeight;}
+}
+
+function processFeatures(features){
+    var keys = Object.keys(features[0].properties);
+    var dataString = keys.join(',')+'\n' + features.reduce(function(acc,curr){
+	acc = acc + keys.reduce(function(ac,cu){
+	    var prop = curr.properties[cu];
+	    if (Array.isArray(prop)) prop = prop.toString().replace(',',':')
+	    ac=ac+prop+',';
+	    return ac;
+	},'') +'\n';
+	return acc;
+    },'');
+    return 'data:text/csv:charset=utf-8,'+dataString;
+}
+
+function saveProps(){
+    saveFile(processFeatures(data),'props.csv');
+}
+function saveSvg(){
+    var header = 'data:image/svg+xml;charset=utf-8,'
+    var content = document.getElementById('map').getElementsByTagName('svg')[0].outerHTML;
+    saveFile(header+content,'map.svg');
+}
+var oas;
+
+function saveFile(content,filename){
+    var encodedUri = encodeURI(content);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
